0a3e96956d1a52abf4be1eaff6864f9c86290039
Jorge Avila <javila@elementum.com>
2018-05-03 21:23:55 -0300

    add progress indicator

---
+++ src/components/progress/index.js
@@ -0,0 +1,35 @@
+import './progress.css';
+export default class Progress {
+  constructor (torrent) {
+    this.torrent = torrent;
+    const template = `<div class="progress-bar-container"><div class="progress-bar"></div></div>`;
+    const temp = document.createElement('div');
+    temp.innerHTML = template;
+    this.$container = temp.querySelector('.progress-bar-container');
+    this.$element = this.$container.querySelector('.progress-bar');
+  }
+  update () {
+    if (this.torrent.progress === 1) {
+      this.destroy()
+    }
+    const progress = !!this.torrent.progress && (this.torrent.progress * 100)
+    this.$element.style.width = `${progress || 0 }%`
+    return this
+  }
+
+  stop() {
+    clearInterval(this.interval)
+  }
+
+  destroy () {
+    this.stop()
+    this.$container.parentNode.removeChild(this.$container)
+  }
+
+  mount (container) {
+    container.prepend(this.$container)
+    const seconds = 2
+    this.interval = setInterval(() => this.update(), seconds * 1000)
+    return this
+  }
+}
