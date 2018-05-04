1b7ba917540e85d1184a0131bd286744c52ab963
Jorge Avila <javila@elementum.com>
2018-05-03 21:26:42 -0300

    add to style

--- src/components/progress/index.js
+++ src/components/progress/index.js
@@ -1,13 +1,14 @@
 import './progress.css';
 export default class Progress {
   constructor (torrent) {
-    this.torrent = torrent;
-    const template = `<div class="progress-bar-container"><div class="progress-bar"></div></div>`;
-    const temp = document.createElement('div');
-    temp.innerHTML = template;
-    this.$container = temp.querySelector('.progress-bar-container');
-    this.$element = this.$container.querySelector('.progress-bar');
+    this.torrent = torrent
+    const template = `<div class="progress-bar-container"><div class="progress-bar"></div></div>`
+    const temp = document.createElement('div')
+    temp.innerHTML = template
+    this.$container = temp.querySelector('.progress-bar-container')
+    this.$element = this.$container.querySelector('.progress-bar')
   }
+
   update () {
     if (this.torrent.progress === 1) {
       this.destroy()
